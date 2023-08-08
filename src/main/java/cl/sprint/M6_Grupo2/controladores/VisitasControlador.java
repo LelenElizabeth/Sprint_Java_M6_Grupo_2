9ac832762b7e15a3c41d250bfc154ae5bb9e447e
Jesufertez <maria.fernandez.cortez@gmail.com>
2023-08-08 14:09:32 -0400

    Se integro crear visitas, se modificó Database y Entity de Visitas y se modificó crear-visita.jsp

--- src/main/java/cl/sprint/M6_Grupo2/controladores/VisitasControlador.java
+++ src/main/java/cl/sprint/M6_Grupo2/controladores/VisitasControlador.java
@@ -3,36 +3,77 @@
  */
 package cl.sprint.M6_Grupo2.controladores;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import cl.sprint.M6_Grupo2.modelos.entity.Cliente;
+import cl.sprint.M6_Grupo2.modelos.entity.Profesional;
 import cl.sprint.M6_Grupo2.modelos.entity.Visitas;
+import cl.sprint.M6_Grupo2.modelos.service.ClienteServicio;
+import cl.sprint.M6_Grupo2.modelos.service.ProfesionalServicio;
 import cl.sprint.M6_Grupo2.modelos.service.VisitasServicio;
-
 
 @Controller
 public class VisitasControlador {
-	
+
 	@Autowired
 	private VisitasServicio vis;
 
-	@RequestMapping(value ="/ListadoVisitas",method = RequestMethod.GET)
+	@Autowired
+	private ProfesionalServicio prof;
+
+	@Autowired
+	private ClienteServicio cliServ;
+
+	@RequestMapping(value = "/ListadoVisitas", method = RequestMethod.GET)
 	public ModelAndView listaVisitas() {
 		ArrayList<Visitas> listaVisitas = new ArrayList<>();
 		listaVisitas = vis.obtenerVisitas();
-		
-		return new ModelAndView ("listado-de-visitas","listaVisitas",listaVisitas);
-	}
-	
-	@RequestMapping(path = "/CrearVisita", method = RequestMethod.GET)
-    public ModelAndView mostrarVista() {
-        return new ModelAndView("crearVisita");
-    }
-	
 
+		return new ModelAndView("listado-de-visitas", "listaVisitas", listaVisitas);
+	}
+
+	@RequestMapping(path = "/crear-visita", method = RequestMethod.GET)
+	public ModelAndView mostrarVista(ModelMap model) {
+		List<Cliente> listaClientes = cliServ.obtenerClientes();
+		model.addAttribute("listaClientes", listaClientes);
+
+		List<Profesional> profesionales = prof.obtenerProfesional();
+		model.addAttribute("profesionales", profesionales);
+		return new ModelAndView("crear-visita", model);
+	}
+
+	@RequestMapping(value="crear-visita",method = RequestMethod.POST)
+	public ModelAndView crearVisita(
+			@RequestParam("cliente_id") int cliente_id,
+			@RequestParam("profesional_id") int profesional_id,
+			@RequestParam("fecha")String fecha,
+			@RequestParam("lugar") String lugar,
+			@RequestParam("comentarios")String comentarios){
+		
+		//Profesional profesional = prof.obtenerProfesional(profesional_id);
+		
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        LocalDateTime fecha_hora = LocalDateTime.parse(fecha, formatter);
+        //Cliente cliente = cliServ.obtenerCliente(cliente_id);
+        //int rut_cliente = cliente.getRut();
+        
+        Visitas visita= new Visitas(cliente_id,fecha_hora,lugar,comentarios, profesional_id);
+		vis.crearVisitas(visita);
+        
+        return new ModelAndView("crear-visita")
+        		.addObject("mensaje","La visita se ha creado correctamente.")
+        		.addObject("MostrarAlert", true);
+        		
+	}
 }
